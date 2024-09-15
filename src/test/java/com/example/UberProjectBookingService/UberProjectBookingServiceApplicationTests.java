8cf675ced5967b4868205ca107ef9265c4e15dce
Manish Kumar <manishkumar@Manishs-MacBook-Pro.local>
2024-09-15 16:06:18 +0530

    Initial project setup

---
+++ src/test/java/com/example/UberProjectBookingService/UberProjectBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UberProjectBookingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberProjectBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
